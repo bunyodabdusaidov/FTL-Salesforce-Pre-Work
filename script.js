45fcdefec43536c8e8336b982db5fe9472cbc5a7
bunyodabdusaidov <bunyodabdusaidov0@gmail.com>
2022-04-18 00:58:58 -0500

    Edited the secret pattern in script.js to use the additional buttons

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const cluePauseTime = 333; //how long to pause in between clues
 const nextClueWaitTime = 1000; //how long to wait before starting playback of the clue sequence
 
 //Global Variables
-var pattern = [2, 2, 4, 3, 2, 1, 2, 4];
+var pattern = [2, 2, 5, 1, 6, 4, 3, 5, 3];
 var progress = 0;
 var gamePlaying = false;
 var tonePlaying = false;
